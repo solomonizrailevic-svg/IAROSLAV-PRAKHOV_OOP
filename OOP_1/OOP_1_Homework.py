8d5d9f8948e86dca3dee01f219c296ec4113b67c
prakh <solomonizrailevic@gmail.com>
2026-05-02 15:40:44 +0300

    OOP_Homework_1

---
+++ OOP_1/OOP_1_Homework.py
@@ -0,0 +1,28 @@
+# class Dog:
+#     "Друг человека"
+#     species = "canis"
+#     legs = 4
+#     name = "Бобик"
+#     age = 2
+# bulldog = Dog()
+# chihuahua = Dog()
+# bulldog.species = "vulpes"
+# chihuahua.legs = 5
+# print(bulldog.species)
+# print(chihuahua.legs)
+# print(bulldog.__dict__)
+# print(chihuahua.__dict__)
+# print(Dog.__doc__)
+# del Dog.name
+# print(Dog.name)
+
+class User:
+    role = "guest"
+    active = True
+setattr(User, 'role', 'admin')
+setattr(User, "email", "ttt@mail.ru")
+print(User.role)
+print(getattr(User, 'active'))
+print(User.email)
+delattr(User, "role")
+print(User.__dict__)
